384199f0edca1f0b1c6d96dc16b70c387eb1e2c1
Cameron Upright <cameron.upright@gmail.com>
2014-02-10 02:07:14 -0700

    Started working on CCP capture of range finder PWM pulse

--- hc-sr04-to-i2c.X/hc-sr04-to-i2c.asm
+++ hc-sr04-to-i2c.X/hc-sr04-to-i2c.asm
@@ -5,33 +5,104 @@
 
 
 
-        CBLOCK 0x20
+        CBLOCK 0x70
+        ; Delay routine variables
         d1
         d2
+
+        ; PWM measurement CCP variables
+        captureRising
+        risingTimeH
+        risingTimeL
+        fallingTimeH
+        fallingTimeL
+
         ENDC
 
 
-        ORG 0x00
+CaptureRising	equ	b'00000101'
+CaptureFalling	equ	b'00000100'
 
+
+
+        ; Starting point
+        ORG 0x000
+        GOTO Init
+
+        ; Interrupt
+        ORG 0x004
+
+        BANKSEL PIR3
+        BCF     PIR3, CCP3IF    ; Clear the intterupt
+
+        RETFIE
+
+
+Init
+
+InitPins
     	BANKSEL	PORTB
         CLRF	PORTB			; Set 0 to all pins
 
     	BANKSEL	TRISB
-        CLRF	TRISB			; All outputs
+        MOVLW   b'00100000'
+        MOVWF   TRISB			; All outputs, except RB5 which is an input
 
-        BANKSEL PORTB
+        BANKSEL APFCON
+        BSF     APFCON, CCP3SEL ; Make sure CCP3 is on RB5
+
+
+InitCCP
+        BANKSEL T1CON
+        MOVLW   b'00000001'
+        MOVWF   T1CON           ; 1:1 prescale, instruction clock, timer1 on
+
+        BANKSEL T1GCON
+        CLRF    T1GCON          ; No gate stuff
+
+        BANKSEL INTCON          ; Enable global and periferal interrupts
+        MOVLW   b'11000000'
+        MOVWF   INTCON
+
+
+
 
 Loop
-        ; Send out a 10us trigger pulse
-        BSF PORTB, 0
-        CALL delay_10us
-        BCF PORTB, 0
 
-        ; Wait for 50ms to start again
-        CALL delay_50ms
+        CALL trigger_pulse
+
+        CALL wait_for_and_record_pwm
+
         GOTO Loop
 
 
+
+trigger_pulse
+
+        BANKSEL PORTB
+        BSF PORTB, 0            ; Send out a 10us trigger pulse
+
+        CALL delay_10us
+
+        BANKSEL PORTB
+        BCF PORTB, 0            ; Stop the trigger pulse
+
+        RETURN
+
+wait_for_and_record_pwm
+        BANKSEL CCP3CON
+        MOVLW   CaptureRising
+        MOVWF   CCP3CON         ; Capture rising edges
+
+        BANKSEL PIE3
+        BSF     PIE3, CCP3IE    ; Enable CCP3 Interrupt
+
+        CALL delay_50ms         ; Wait for 50ms to start again
+
+        BANKSEL PIE3
+        BCF     PIE3, CCP3IE    ; Disable CCP3 interrupt
+
+        RETURN
 
 
 
